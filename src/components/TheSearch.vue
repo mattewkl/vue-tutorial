f24f17a378086c6d1b7e0198dd87d8a8f3df7dab
mattewkl <cikle290@gmail.com>
2023-07-16 22:09:27 +1000

    42 done

--- src/components/TheSearch.vue
+++ src/components/TheSearch.vue
@@ -1,16 +1,45 @@
 <script>
-import TheSearchBtn from './TheSearchBtn.vue';
+import TheSearchBtn from "./TheSearchBtn.vue";
 import TheSearchInput from "./TheSearchInput.vue";
-import TheSearchResults from './TheSearchResults.vue';
+import TheSearchResults from "./TheSearchResults.vue";
 
 export default {
-  components: { TheSearchBtn, TheSearchInput, TheSearchResults },};
+  components: { TheSearchBtn, TheSearchInput, TheSearchResults },
+  data() {
+    return {
+      keywords: [
+        "new york giants",
+        "new york alicia keys",
+        "new york giants vs washington football",
+        "new york",
+        "new york song",
+        "new york new york frank sinatra",
+        "new york jets",
+        "new york city",
+        "new york giants live",
+        "new york state of mind",
+        "new york giants vs washington football live",
+        "new york giants injury",
+        "new york giants live stream",
+        "new york accent",
+      ],
+      query: "",
+    };
+  },
+  computed: {
+    results() {
+      return this.keywords.filter((result) => {
+        return result.includes(this.query);
+      });
+    },
+  },
+};
 </script>
 <template>
-  <div class=" mr-2 flex w-full ">
-    <TheSearchInput />
-    <TheSearchResults/>
-    <TheSearchBtn/>
+  <div class="mr-2 flex w-full">
+    <TheSearchInput v-model:query="query" />
+    <TheSearchResults v-show="query" :results="results" />
+    <TheSearchBtn />
   </div>
 </template>
 
--- src/components/TheSearchInput.vue
+++ src/components/TheSearchInput.vue
@@ -1,41 +1,49 @@
 <template>
   <div class="relative w-full">
-    <input type="text" placeholder="Search" :class="classes" />
+    <input
+      type="text"
+      placeholder="Search"
+      :class="classes"
+      :value="query" 
+      @input="$emit('update:query', $event.target.value)"
+    />
     <button class="absolute top-0 right-0 h-full px-3 focus:outline-none">
-      <BaseIcon name="x" class="w-5 w-5" />
+      <BaseIcon name="x" class="w-5 w-5" v-show="modelValue" @click="$emit('update:query', '')"/>
     </button>
   </div>
 </template>
 
 <script>
-import BaseIcon from './BaseIcon.vue'
+import BaseIcon from "./BaseIcon.vue";
 
 export default {
   components: {
-    BaseIcon
+    BaseIcon,
   },
+  props: ['query'],
+  emits: ['update:query'],
 
-  data () {
+  data() {
     return {
       classes: [
-        'w-full',
-        'h-full',
-        'px-3',
-        'shadow-inner',
-        'rounded-bl-sm',
-        'rounded-tl-sm',
-        'border',
-        'border-gray-300',
-        'focus:border-blue-700',
-        'focus:outline-none'
-      ]
-    }
+        "w-full",
+        "h-full",
+        "px-3",
+        "shadow-inner",
+        "rounded-bl-sm",
+        "rounded-tl-sm",
+        "border",
+        "border-gray-300",
+        "focus:border-blue-700",
+        "focus:outline-none",
+      ],
+    };
   },
 
-  mounted () {
+  mounted() {
     if (window.innerWidth < 640) {
-      this.$el.focus()
+      this.$el.focus();
     }
-  }
-}
+  },
+};
 </script>
